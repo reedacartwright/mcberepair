98f693d297042e8a3e70a0686e3f4b0e1e588c04
Reed A. Cartwright <cartwright@asu.edu>
2020-04-27 20:28:43 -0700

    Try again to run lcov on circleci

--- .ci/lcov.bash
+++ .ci/lcov.bash
@@ -1,17 +1,24 @@
 # download latest lcov
 lcov_commit="40580cd65909bc8324ae09b36bca2e178652ff3f"
 
-if [ ! -f lcov/bin/lcov ]; then
+cd $1
+
+if [[ ! -f ./lcov/bin/lcov ]]; then
     mkdir -p lcov
     curl -sL  https://github.com/linux-test-project/lcov/archive/${lcov_commit}.tar.gz |
     tar --strip-components=1 -xz -C lcov
 fi
 
-lcov/bin/lcov --directory . --capture --output-file coverage.info
-lcov/bin/lcov --remove coverage.info '/usr/*' '*/leveldb-mcpe/*' '*/tests/*' '*/examples/*' '*/contrib/*' --output-file coverage.info
+lcov_dir=$(realpath ./lcov/bin)
+
+PATH="${PATH}:${lcov_dir}"
+
+# Run lcov
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info '/usr/*' '*/leveldb-mcpe/*' '*/tests/*' '*/examples/*' '*/contrib/*' --output-file coverage.info
 
 # Output summary to console
-lcov/bin/lcov --list coverage.info 
+lcov --list coverage.info 
 
 # Upload report to CodeCov
 #bash <(curl -s https://codecov.io/bash) || echo "CodeCov failed to collect reports."
